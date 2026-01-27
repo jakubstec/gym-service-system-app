842df6de49a9b3f36be8d43e971121f0e03bb3bd
jakubstec <kub9stec@gmail.com>
2026-01-27 11:12:15 +0100

    change hours in timetable

--- src/gym_app/Services/Initialization/GymScheduleInitializer.cs
+++ src/gym_app/Services/Initialization/GymScheduleInitializer.cs
@@ -20,25 +20,24 @@ public static class GymScheduleInitializer
                 switch (dayOfWeek)
                 {
                     case 1:
-                        sessions.Add(SessionFactory.CreateGroupClass("Joga Poranna", "Ola", dayOffset, 9.0, 60));
-                        sessions.Add(SessionFactory.CreateGroupClass("Zumba", "Kasia", dayOffset, 18.0, 60));
+                        sessions.Add(SessionFactory.CreateGroupClass("Joga Poranna", "Ola", dayOffset, 9.0, 90));
+                        sessions.Add(SessionFactory.CreateGroupClass("Zumba", "Kasia", dayOffset, 18.0, 90));
                         break;
                     case 2:
-                        sessions.Add(SessionFactory.CreateGroupClass("Pilates", "Ola", dayOffset, 10.5, 60));
+                        sessions.Add(SessionFactory.CreateGroupClass("Pilates", "Ola", dayOffset, 10.5, 120));
                         sessions.Add(SessionFactory.CreateGroupClass("Crossfit", "Marek", dayOffset, 19.0, 90));
                         break;
                     case 3:
                         sessions.Add(SessionFactory.CreateGroupClass("Rowery", "Ola", dayOffset, 12.0, 90));
-                        sessions.Add(SessionFactory.CreateGroupClass("HIIT Express", "Kasia", dayOffset, 14.0, 60));
+                        sessions.Add(SessionFactory.CreateGroupClass("HIIT Express", "Kasia", dayOffset, 14.0, 120));
                         break;
                     case 4:
-                        sessions.Add(SessionFactory.CreateGroupClass("Pilates", "Ola", dayOffset, 10.5, 60));
+                        sessions.Add(SessionFactory.CreateGroupClass("Pilates", "Ola", dayOffset, 10.5, 120));
                         sessions.Add(SessionFactory.CreateGroupClass("Spinning", "Marek", dayOffset, 16.5, 90));
                         break;
                     case 5:
-                        sessions.Add(SessionFactory.CreateGroupClass("Stretching", "Ola", dayOffset, 9.0, 60));
+                        sessions.Add(SessionFactory.CreateGroupClass("Stretching", "Ola", dayOffset, 9.0, 90));
                         sessions.Add(SessionFactory.CreateGroupClass("Zumba Party", "Kasia", dayOffset, 18.0, 120));
-                        break;
                 }
 
                 sessions.Add(SessionFactory.CreatePersonalTraining("Trening Personalny", "Piotr", dayOffset, 8.0));
